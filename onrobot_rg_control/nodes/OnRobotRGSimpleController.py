fca9dec930e6832718eb9e0375577a49f0ebf491
Patrick Reitz <patrick-reitz@t-online.de>
2022-06-23 13:44:07 +0200

    added an option to the simple controller to set the force to a value of choice

--- onrobot_rg_control/nodes/OnRobotRGSimpleController.py
+++ onrobot_rg_control/nodes/OnRobotRGSimpleController.py
@@ -24,35 +24,37 @@ def genCommand(char, command):
         command.rCTR = 1
         command.outZero = 1
     if char == 'c':
-    #    command.rGFR = 400
         command.rGWD = 0
         command.rCTR = 1
     elif char == 'o':
-    #    command.rGFR = 400
         command.rGWD = max_width
         command.rCTR = 1
     elif char == 'i':
         command.rGFR += 25
         command.rGFR = min(max_force, command.rGFR)
-        command.rCTR = 1
     elif char == 'd':
         command.rGFR -= 25
         command.rGFR = max(0, command.rGFR)
-        command.rCTR = 1
     elif char == 's':
         command.rCTR = 0
     elif char == 'z':
         command.outZero = 1
     elif char == 'uz':
         command.outZero = 0
-    else:
-        # If the command entered is a int, assign this value to rGWD
-        try:
-        #    command.rGFR = 400
-            command.rGWD = min(max_width, int(char))
-            command.rCTR = 1
-        except ValueError:
-            pass
+    elif char != '': 
+        if char[0] == 'f':
+            try:
+                command.rGFR = max(0, min(max_force, int(char[1:])))
+                command.rCTR = 1
+            except ValueError:
+                pass
+        else:
+            # If the command entered is a int, assign this value to rGWD
+            try:
+                command.rGWD = min(max_width, int(char))
+                command.rCTR = 1
+            except ValueError:
+                pass
 
     return command
 
@@ -78,6 +80,7 @@ def askForCommand(command):
     strAskForCommand += 'd: Decrease force\n'
     strAskForCommand += 'z: sets the outZero bit to 1, so all force and torque values are set to 0\n'
     strAskForCommand += 'uz: sets the outZero bit to 0, to undo the force and torque bias\n'
+    strAskForCommand += 'f0 to f400: Set the force value between 0N and 40N (very low values might result in the gripper not moving)\n'
 
     strAskForCommand += '-->'
 
